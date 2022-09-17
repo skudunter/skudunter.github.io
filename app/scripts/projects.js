a600fb6d0b62fe101d2b345c2d8116064f134eec
Daniel <danielgruttercpt@gmail.com>
2022-09-17 12:16:43 +0200

    animations complete

--- app/scripts/projects.js
+++ app/scripts/projects.js
@@ -5,19 +5,35 @@ const projects = [
 const display = document.getElementById("display");
 let index = 0;
 function forward() {
+  display.className =
+    "mx-auto w-9/12 h-full overflow-hidden transition-none ease-in-out duration-1000";
+  display.style.opacity = 0;
   if (index + 1 < projects.length) {
     index++;
   } else {
     index = 0;
   }
   display.setAttribute("src", projects[index]);
+  setInterval(() => {
+    display.className =
+      "mx-auto w-9/12 h-full overflow-hidden transition ease-in-out duration-1000";
+    display.style.opacity = 1;
+  },0);
 }
 
 function backward() {
+  display.className =
+    "mx-auto w-9/12 h-full overflow-hidden transition-none ease-in-out duration-1000";
+  display.style.opacity = 0;
   if (index > 0) {
-    index --;
+    index--;
   } else {
-    index = projects.length-1;
+    index = projects.length - 1;
   }
   display.setAttribute("src", projects[index]);
+  setInterval(() => {
+    display.className =
+      "mx-auto w-9/12 h-full overflow-hidden transition ease-in-out duration-1000";
+    display.style.opacity = 1;
+  },0);
 }
