c4295d8c2c49d3229553ec6d3fd51c7eeca74551
Daniel <danielgruttercpt@gmail.com>
2022-09-17 21:59:35 +0200

    fixed canvas resizing

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,34 +1,48 @@
 var canvas = document.getElementById("gameCanvas");
 var ctx = canvas.getContext("2d");
 const body = document.body;
+let width = window.innerWidth / 40;
+let height = window.innerHeight / 5;
+let radius = window.innerHeight / 9 / 6 + window.innerWidth / 16 / 6;
 ctx.fillStyle = "#ABEBD2";
+
 canvas.width = window.outerWidth;
 canvas.height = window.innerHeight - body.scrollHeight;
+
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 addEventListener("resize", () => {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight - body.scrollHeight;
-  p1 = new paddle(10, canvas.height / 2 - 120);
-  p2 = new paddle(canvas.width - 60, canvas.height / 2 - 120);
-  bll = new ball(canvas.width / 2 + 10, canvas.height / 2 - 50);
+
+  p1 = new paddle(10, canvas.height / 2 - height / 2);
+  p2 = new paddle(canvas.width - width - 10, canvas.height / 2 - height / 2);
+  bll = new ball(canvas.width / 2, canvas.height / 2);
+
+  width = canvas.width / 40;
+  if (width < 40) {
+    width = 40;
+  }
+  height = canvas.height / 5;
+  if (height < 100) {
+    height = 120;
+  }
 });
+
 class paddle {
   constructor(x, y) {
     this.y = y;
     this.x = x;
-    this.width = 50;
-    this.height = 180;
   }
   show() {
     ctx.fillStyle = "#f8fafc";
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.fillRect(this.x, this.y, width, height);
   }
 }
 class ball {
   constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.r = 30;
+    this.r = radius;
     if (Math.random() >= 0.5) {
       this.vx = -8;
     } else {
@@ -47,9 +61,9 @@ class ball {
     this.y += this.vy;
   }
   collide() {
-    if (this.x + this.r > canvas.width - 60) {
+    if (this.x + this.r > canvas.width - 10 - width) {
       this.vx = -this.vx;
-    } else if (this.x - this.r < 60) {
+    } else if (this.x - this.r < 10 + width) {
       this.vx = -this.vx;
     }
     if (this.y + this.r > canvas.height) {
@@ -60,9 +74,10 @@ class ball {
   }
 }
 
-let p1 = new paddle(10, canvas.height / 2 - 120);
-let p2 = new paddle(canvas.width - 60, canvas.height / 2 - 120);
-let bll = new ball(canvas.width / 2 + 10, canvas.height / 2 - 50);
+let p1 = new paddle(10, canvas.height / 2 - height / 2);
+let p2 = new paddle(canvas.width - width - 10, canvas.height / 2 - height / 2);
+let bll = new ball(canvas.width / 2, canvas.height / 2);
+
 p1.show();
 p2.show();
 bll.show();
@@ -76,13 +91,14 @@ function playGame() {
   p2.show();
   bll.show();
   bll.collide();
-  p1.y = bll.y - 90;
-  p2.y = bll.y - 90;
+  p1.y = bll.y - height / 2;
+  p2.y = bll.y - height / 2;
   for (let i = 0; i < canvas.height / 40; i++) {
     ctx.fillRect(canvas.width / 2 - 10, i * 40, 10, 30);
   }
-  ctx.font = "120px Arial";
-  ctx.fillText("Skudunter LLC", canvas.width / 2 - 570, canvas.height / 2);
+  ctx.font = (canvas.width/18 + "px Arial");
+  ctx.textAlign = "center";
+  ctx.fillText("Skudunter LLC", canvas.width / 2 - canvas.width/12.5, canvas.height / 2);
   requestAnimationFrame(playGame);
 }
 
