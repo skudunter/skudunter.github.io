a9f0f50b18c6edda5cd384fd42e8d25977036282
Daniel <danielgruttercpt@gmail.com>
2022-09-18 14:03:05 +0200

    final commit

--- app/scripts/projects.js
+++ app/scripts/projects.js
@@ -5,8 +5,6 @@ const projects = [
 const display = document.getElementById("display");
 let index = 0;
 function forward() {
-   display.className =
-     "absolute m-auto h-full w-4/6 p-8 right-17% bg-center bg-tersiary rounded-3xl transition ease-in-out duration-1000";
   display.style.opacity = 0;
   if (index + 1 < projects.length) {
     index++;
@@ -14,27 +12,14 @@ function forward() {
     index = 0;
   }
   display.setAttribute("src", projects[index]);
-  setInterval(() => {
-    display.className =
-      "absolute m-auto inline h-full w-4/6 p-8 right-17% bg-center bg-tersiary rounded-3xl transition ease-in-out duration-1000";
-    display.style.opacity = 1;
-  },0);
 }
 
 function backward() {
-  display.className =
-    "absolute m-auto  h-full w-4/6 p-8 right-17% bg-center bg-tersiary rounded-3xl transition ease-in-out duration-1000";
-  display.style.opacity = 0;
   if (index > 0) {
     index--;
   } else {
     index = projects.length - 1;
   }
   display.setAttribute("src", projects[index]);
-  setInterval(() => {
-    display.className =
-      "absolute m-auto inline h-full w-4/6 p-8 right-17% bg-center bg-tersiary rounded-3xl transition ease-in-out duration-1000";
-    display.style.opacity = 1;
-  },0);
 }
 
